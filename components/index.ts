faf2fb768e069e63dc91ec9e56cf673817303cc8
Maxigong <maxi@nolgong.com>
2022-09-17 23:14:25 +0900

    imoprt

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,8 @@
 import FormRow from "./ui/FormRow";
 import SelectRow from "./ui/SelectRow";
 
-import EditLinks from "./admin/links/EditLinks";
-import LinksModal from "./admin/links/LinksModal";
+import EditLinks from "./admin/Links/EditLinks";
+import LinksModal from "./admin/Links/LinksModal";
 
 import MyLinks from "./home/MyLinks";
 
