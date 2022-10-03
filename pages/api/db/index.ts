b8485f1823a295de14f59c0ec25798f7b551140a
maxrpark <proyectoindit@gmail.com>
2022-10-03 16:22:49 +0900

    api

--- pages/api/db/index.ts
+++ pages/api/db/index.ts
@@ -4,16 +4,20 @@ import { getToken } from "next-auth/jwt";
 
 const handler: NextApiHandler = async (req, res) => {
   const token = await getToken({ req });
+  console.log(token);
+
   if (!token || token.email !== process.env.ADMINISTRATOR) {
     return res.status(401).json({ msg: "Unauthorized to visit this route" });
   }
 
   if (req.method === "POST") {
-    const { data, fileName } = JSON.parse(req.body);
-    fs.writeFileSync(`./db/${fileName}.json`, JSON.stringify(data));
-    return res.status(200).json({ msg: "succeeded" });
-    // const { data, fileName } = req.body;
-    // fs.writeFileSync(`./db/${fileName}.json`, JSON.stringify(data));
+    try {
+      const { data, fileName } = JSON.parse(req.body);
+      fs.writeFileSync(`./db/${fileName}.json`, JSON.stringify(data));
+      return res.status(200).json({ msg: "succeeded" });
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
 };
 
