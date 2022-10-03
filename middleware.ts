0187d96c689f8cbd5c7cd0dd1c0534fd0f46887e
maxrpark <proyectoindit@gmail.com>
2022-10-03 16:12:15 +0900

    midd

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,19 @@ import withAuth from "next-auth/middleware";
 import type { NextRequest } from "next/server";
 
 export default withAuth(
+  //
   function middleware(req: NextRequest) {
     if (req.nextUrl.pathname.startsWith("/admin")) {
-      console.log("hello");
+      // This logic is only applied to /admin
     }
   },
   {
     callbacks: {
       authorized: ({ token }) => {
         if (token && token.email === process.env.ADMINISTRATOR) {
-          console.log(token);
-
           return true;
         }
-        return true;
+        return false;
       },
     },
   }
